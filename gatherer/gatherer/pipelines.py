7518e961d3e799fdbfcef026cd8fac67a44d2a6a
Benny Webster <bwebster1992@gmail.com>
2023-04-11 20:17:18 +1000

    Batched db write on close_spider

--- gatherer/gatherer/pipelines.py
+++ gatherer/gatherer/pipelines.py
@@ -3,7 +3,9 @@
 import os
 import logging
 import psycopg2
-from datetime import datetime, timezone, timedelta
+import pandas as pd
+from datetime import datetime, timezone
+from io import StringIO
 
 from psycopg2.extras import Json
 from psycopg2.extensions import register_adapter
@@ -17,6 +19,7 @@ PGDATABASE = os.environ['PGDATABASE']
 
 
 class GathererPipeline:
+
     def open_spider(self, spider):
         logging.debug('Connecting to db...')
         try:
@@ -27,78 +30,78 @@ class GathererPipeline:
                 dbname=PGDATABASE
             )
             self.cur = self.connection.cursor()
-        except:
-            logging.debug('Db connection error.')
+        except (Exception, psycopg2.DatabaseError) as error:
+            logging.debug("Error: %s" % error)
+            self.conn.rollback()
+            return 1
 
         # Record scrape details in db
-        self.cur.execute(
-            """
-            INSERT INTO scrape(
-              scrape_id, scrape_params, spider_name,
-              completed, start_time
+        logging.debug('Writing scrape metadata to db...')
+        try:
+            self.cur.execute(
+                """
+                INSERT INTO scrape(
+                scrape_id, scrape_params, spider_name,
+                completed, start_time
+                )
+                VALUES (%s, %s, %s, false, %s)
+                """,
+                (
+                    spider.payload['scrape_id'],
+                    Json(spider.payload['scrape_params']),
+                    spider.name, datetime.now(timezone.utc)
+                ),
             )
-            VALUES (%s, %s, %s, false, %s)
-            """,
-            (
-                spider.payload['scrape_id'],
-                Json(spider.payload['scrape_params']),
-                spider.name, datetime.now(timezone.utc)
-            ),
-        )
-        self.connection.commit()
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            logging.debug("Error: %s" % error)
+            self.conn.rollback()
+            return 1
+
+        # Initalise items container
+        self.item_list = []
 
     def process_item(self, item, spider):
-        try:
-            logging.debug('Writing item to db...')
-
-            if spider.name == 'sf-forecast':
-                self.cur.execute(
-                    """
-                    INSERT INTO forecast(
-                        last_updated, break_id, forecast_time, swell_info,
-                        wind_speed, wind_direction, wind_state
-                        )
-                    VALUES(%s,%s,%s,%s,%s,%s,%s)
-                    """,
-                    (item['last_updated'], item['break_id'], item['forecast_time'],
-                     item['swell_info'], item['wind_speed'], item['wind_direction'],
-                     item['wind_state'])
-                )
-                self.connection.commit()
-
-            if spider.name == 'sf-breaks':
-                self.cur.execute(
-                    """
-                    INSERT INTO breaks(
-                        region, break_id
-                        )
-                    VALUES(%s,%s)
-                    """,
-                    (item['region'], item['break_id'])
-                )
-                self.connection.commit()
-
-            logging.debug('Db write success.')
-        except:
-            logging.debug('Db write error.')
-        finally:
-            return item
+        self.item_list.append(item)
+        return item
 
     def close_spider(self, spider):
-        # Update spider status in db
-        self.cur.execute(
-            """
-            UPDATE scrape
-            SET completed = true,
-                end_time = (%s)
-            WHERE scrape_id = (%s)
-            """,
-            (
-                datetime.now(timezone.utc),
-                spider.payload['scrape_id']
+        logging.debug('Writing scraped items to db...')
+
+        # Collect items in df
+        items_df = pd.DataFrame(self.item_list)
+        items_table = spider.name.replace('sf-', '')
+
+        # Load df into buffer
+        buffer = StringIO()
+        items_df.to_csv(buffer, index=False, header=False)
+        buffer.seek(0)
+
+        try:
+            # Copy from buffer to db
+            self.cur.copy_from(buffer, items_table, sep=",")
+
+            # Update spider status in db
+            logging.debug('Updating spider metadata...')
+            self.cur.execute(
+                """
+                UPDATE scrape
+                SET completed = true,
+                    end_time = (%s)
+                WHERE scrape_id = (%s)
+                """,
+                (
+                    datetime.now(timezone.utc),
+                    spider.payload['scrape_id']
+                )
             )
-        )
-        self.connection.commit()
+            # Commit changes
+            self.connection.commit()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logging.debug("Error: %s" % error)
+            self.conn.rollback()
+            return 1
 
         # Close db connection
         self.cur.close()
